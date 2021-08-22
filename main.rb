3257c866dd80478a06261feeba3b9c756dbc730e
Archesper <pk4nde@gmail.com>
2021-08-22 19:32:00 +0100

    Finish Board implementation, Add Game class

--- main.rb
+++ main.rb
@@ -2,13 +2,30 @@
 
 require_relative 'tictactoe'
 
-puts 'What would you (Player 1) like to play as? (X, O, etc...)'
-player1 = Player.new(gets, 'Player 1')
+# Helper method to check if a string is numeric
+def numeric_string?(string)
+  if string.to_f != 0.0 || (string.to_i.zero? && ['0', '0.0'].include?(string))
+    true
+  else
+    false
+  end
+end
+
+puts 'What would you (Player 1) like to play as? (X, O, etc...)' \
+     ' You may not pick a number.'
+sigil1 = gets.chomp[0]
+while numeric_string?(sigil1)
+  puts 'You may not pick a number'
+  sigil1 = gets.chomp[0]
+end
+player1 = Player.new(sigil1, 'Player 1')
 puts 'What would you (Player 2) like to play as? (X, O, etc...). You may not' \
-" play as #{player1.sigil}:"
-sigil2 = gets
-while sigil2 == player1.sigil
-  puts 'Please pick another sigil'
-  sigil2 = gets
+ " play as '#{player1.sigil}'.".chomp
+sigil2 = gets.chomp
+while sigil2 == (player1.sigil) || numeric_string?(sigil2)
+  puts numeric_string?(sigil2) ? 'You may not pick a number:' : 'Please pick another character:'
+  sigil2 = gets.chomp
 end
 player2 = Player.new(sigil2, 'Player 2')
+
+game = Game.new(player1, player2)
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: false
+require_relative 'helper'
 
 # Player class
 class Player
@@ -33,13 +34,27 @@ class Board
 
   # Returns winner's sigil if there is one, nil otherwise
   def winner_sigil
-    # Vertical check
-    3.times do |i|
-      if board[i] == board[i + 3] && board[i + 3] == board[i + 6]
-        return board[i]
-      end
-    end
-    # Horizontal check
+    return diagonal_win_check if diagonal_win_check
+    return horizontal_win_check if horizontal_win_check
+    return vertical_win_check if vertical_win_check
+  end
+
+  # String representation of the board
+  def to_s
+    <<-BOARD
+     #{cell_to_s(1)} | #{cell_to_s(2)} | #{cell_to_s(3)}
+    ---+---+---
+     #{cell_to_s(4)} | #{cell_to_s(5)} | #{cell_to_s(6)}
+    ---+---+---
+     #{cell_to_s(7)} | #{cell_to_s(8)} | #{cell_to_s(9)}
+    BOARD
+  end
+
+  private
+
+  # Checks for horizontal win, returns winner's sigil if there is one,
+  # nil otherwise
+  def horizontal_win_check
     i = 0
     while i <= 6
       if board[i] == board[i + 1] && board[i + 1] == board[i + 2]
@@ -48,10 +63,42 @@ class Board
         i += 3
       end
     end
-    # Diagonal check
+
+    nil
+  end
+
+  # Checks for vertical win, returns winner's sigil if there is one,
+  # nil otherwise
+  def vertical_win_check
+    3.times do |i|
+      return board[i] if board[i] == board[i + 3] && board[i + 3] == board[i + 6]
+
+      nil
+    end
+  end
+
+  # Checks for diagonal win, returns winner's sigil if there is one,
+  # nil otherwise
+  def diagonal_win_check
     if (board[0] == board[4] && board[4] == board[8]) ||
        (board[2] == board[4] && board[4] == board[6])
-      board[4]
+      return board[4]
     end
+
+    nil
+  end
+
+  # Returns a string representation of a board cell - its index if it's empty,
+  # its sigil otherwise
+  def cell_to_s(index)
+    board[index] || index
+  end
+end
+
+# Game class
+class Game
+  def initialize(player1, player2)
+    @board = Board.new
+    @players = [player1, player2]
   end
 end
